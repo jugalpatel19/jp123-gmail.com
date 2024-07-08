de521cf1804e8ec0262ccc9dbf2bc08824121c31
jugalpatel31 <jugal1981986@gmail.com>
2024-07-08 11:57:37 +0530

    ll

--- Assignment/src/Module2/IndexArray.java
+++ Assignment/src/Module2/IndexArray.java
@@ -8,20 +8,16 @@ public class IndexArray {
 	  public static int findIndex(int arr[], int t) 
 	    { 
 	  
-	        // if array is Null 
+	       
 	        if (arr == null) { 
 	            return -1; 
 	        } 
-	  
-	        // find length of array 
-	        int len = arr.length; 
+	  int len = arr.length; 
 	        int i = 0; 
 	  
-	        // traverse in the array 
 	        while (i < len) { 
 	  
-	            // if the i-th element is t 
-	            // then return the index 
+	           
 	            if (arr[i] == t) { 
 	                return i; 
 	            } 
@@ -34,11 +30,11 @@ public class IndexArray {
 public static void main(String[] args) {
 	 int[] my_array = { 5, 4, 6, 1, 3, 2, 7, 8, 9 }; 
 	  
-     // find the index of 5 
+    
      System.out.println("Index position of 5 is: "
                         + findIndex(my_array, 5)); 
 
-     // find the index of 7 
+   
      System.out.println("Index position of 7 is: "
                         + findIndex(my_array, 7)); 
  } 
