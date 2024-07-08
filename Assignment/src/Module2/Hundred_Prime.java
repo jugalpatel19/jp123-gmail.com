5d405f337812558a04f6df48374533be7d8cce6d
jugalpatel31 <jugal1981986@gmail.com>
2024-07-08 13:03:57 +0530

    ll

--- Assignment/src/Module2/Hundred_Prime.java
+++ Assignment/src/Module2/Hundred_Prime.java
@@ -8,29 +8,28 @@ package Module2;
 public class Hundred_Prime {
 public static void main(String[] args) {
 	
-    int i, number = 1, count, sum =0; {
-			
-			while(number <= 100)
-			{
-				count = 0;
-				i = 2;
-			    while(i <= number/2 )
-			    {
-			    	if(number % i == 0)
-			    	{
-			    		count++;
-			    		break;
-			    	}
-			    	i++;
-			    }
-			    if(count == 0 && number != 1 )
-			    {
-			    	sum = sum + number;
-			    }
-			    number++;
+    int i,j,sum=0; 
+    
+    for (i=1;i<=100;i++) {
+    	int count=0;
+    	for(j=2;j<=i/2;j++) {
+    		if (i%j==0) {
+    			count++;
+    			break;
+    		}
+    		
+    		
 			}
-	                System.out.println("\n The Sum of Prime Numbers from 1 to 100 = " + sum);
+    	if (count==0&& i!=1) {
+    		 System.out.println("Prime Numbers :" +i);
+    		sum=sum+i;
+    		
+    	}
+
+    }
+    System.out.println("prime number sum:"+sum);
+
+	              
 		}
-}
 }
 
